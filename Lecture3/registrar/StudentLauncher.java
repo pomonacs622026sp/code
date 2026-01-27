db0fbce7753c1a39c49b19fde51b9bca22e28f96
radiolarian <noon@berkeley.edu>
2026-01-27 11:11:09 -0800

    fix studentlauncher w worksheet answers

--- Lecture3/registrar/StudentLauncher.java
+++ Lecture3/registrar/StudentLauncher.java
@@ -1,4 +1,4 @@
-package Registrar;
+package registrar;
 
 public class StudentLauncher {
 
@@ -7,14 +7,14 @@ public class StudentLauncher {
         PomonaStudent student1 = new PomonaStudent("cecil");
         student1.setYearEntered(2022);
         System.out.println(student1.getYearEntered());
-        System.out.println(student1.yearEntered);
-        PomonaStudent.graduateAllStudents();
+        System.out.println(student1.getYearEntered());
+        // PomonaStudent.graduateAllStudents();
 
         ScrippsStudent student2 = new ScrippsStudent("La Semeuse");
         student2.setYearEntered(1889);
         System.out.println(student1);
         System.out.println(student2);
-        PomonaStudent.olderStudent(student1, student2);
+        // PomonaStudent.olderStudent(student1, student2);
     }
 
 }
